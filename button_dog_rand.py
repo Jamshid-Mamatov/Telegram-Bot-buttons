9d901b340ed432b31884d9cb71c7daf60fa0cf6e
Jamshid <mamatovjamshid506@gmail.com>
2021-02-08 17:53:29 +0500

    update function

--- button_dog_rand.py
+++ button_dog_rand.py
@@ -2,4 +2,16 @@ import requests
 import json
 import os
 from pprint import pprint
+token=os.environ['token']
+
+def update():
+    url=f"https://api.telegram.org/bot{token}/getUpdates"
+    r=requests.get(url)
+
+    data=r.json()['result']
+    text=data[-1]['message']['text']
+    chat_id=data[-1]['message']['chat']['id']
+    inform=[text,chat_id]
+
+    return inform
 
