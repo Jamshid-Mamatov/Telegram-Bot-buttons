c3025fd16f3a1ef91a63b869dd914d480a3f18ff
Jamshid <mamatovjamshid506@gmail.com>
2021-02-08 16:37:27 +0500

    button with A,B,C,D

---
+++ button_3.py
@@ -0,0 +1,43 @@
+import requests
+import json 
+import os
+from pprint import pprint
+
+token=os.environ['token']
+
+
+ids=[]
+
+def update():
+    url=f"https://api.telegram.org/bot{token}/getUpdates"
+    r=requests.get(url)
+
+    data=r.json()
+    
+    chat_id=data['result'][-1]['message']['chat']['id']
+    
+
+    return chat_id
+
+
+def send_keyboard(id):
+    url=f"https://api.telegram.org/bot{token}/sendMessage"
+    payload={
+        'chat_id':id,
+        'text':"button",
+        'reply_markup':{
+            'keyboard':[['A','B'],['C','D']]
+        }
+    }
+
+    r=requests.get(url,json=payload)
+    return r.json
+
+while True:
+
+    chat_id=update()
+    
+    
+    if chat_id not in  ids:
+        pprint(send_keyboard(chat_id))
+        ids.append(chat_id)
